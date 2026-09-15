063a89f20cdf2f760115c75d64f3ce9b671f7a5b
rickyjiang88 <gruppojm@gmail.com>
2026-09-15 02:15:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-const toggle = document.querySelector('.toggle');
 const nav = document.querySelector('header nav');
-
-if (toggle && nav) {
-  toggle.addEventListener('click', () => {
-    const isOpen = nav.classList.toggle('open');
-    toggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
-  });
-
+const toggle = document.querySelector('.toggle');
+if (nav && toggle) {
   nav.querySelectorAll('a').forEach((a) => {
     a.addEventListener('click', () => {
       nav.classList.remove('open');
